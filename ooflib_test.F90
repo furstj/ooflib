dbefd3ebb8a777a76a8b75556a4a03da56f3b632
Jiri Furst <Jiri.Furst@gmail.com>
2013-05-26 13:57:48 +0200

    Support for rational numbers

--- ooflib_test.F90
+++ ooflib_test.F90
@@ -2,8 +2,10 @@ program ooflib_test
 
   use array_iterators, only: array_iterators_test
   use lists, only : lists_test
+  use rationals, only : rationals_test
 
   call array_iterators_test()
   call lists_test()
+  call rationals_test()
 
 end program ooflib_test
